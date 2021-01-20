3cebff748dac7d31a577e3c2f30703449c8af326
Katambo <ykatambo@ANYACCESS.NET>
2021-01-20 17:08:37 +0200

    installed packages and register services in startup

--- GoodBooks.Web/Startup.cs
+++ GoodBooks.Web/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using GoodBooks.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace GoodBooks.Web
@@ -28,6 +23,10 @@ namespace GoodBooks.Web
         {
 
             services.AddControllers();
+            services.AddDbContext<GoodBooksDbContext>(options => {
+                options.EnableDetailedErrors();
+                options.UseNpgsql(Configuration.GetConnectionString("goodbooks.dev"));
+            });
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "GoodBooks.Web", Version = "v1" });
